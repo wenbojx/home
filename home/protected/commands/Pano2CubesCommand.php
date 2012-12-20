74a54cc1d2d28ba1b419642fd02f3dbea3ad361c
wenbojx <wenbojx@sina.com>
2012-12-20 23:54:25 +0800

    a

--- home/protected/commands/Pano2CubesCommand.php
+++ home/protected/commands/Pano2CubesCommand.php
@@ -1,31 +1,31 @@
 <?php 
 ini_set('memory_limit', '100M');
 class Pano2CubesCommand extends CConsoleCommand {
-	
+
 	public $width = '3724';  //cube
 	public $swidth = '11700'; //sphere
 	public $sheight = '5850'; //sphere
-	
+
 	private $linux_path_prefix = '/var/www/home/home';
 	private $linux_pttool_path = '/usr/local/libpano13/bin/PTmender';
 	//public $linux_shpere_prefix = '/tmp';
 	private $script_num = '';
 	private $script_path = '';
-	
+
 	public function actionRun(){
-		
+
 		$scene_ids = $this->get_queue_list();
 		//print_r($scene_ids);
 		if(!$scene_ids){
 			return false;
 		}
-		
+
 		$pano_pics = $this->get_pano_pic_path($scene_ids);
 		//print_r($pano_pics);
 		if(!$pano_pics){
 			return false;
 		}
-		
+
 		$num = $this->get_script_path();
 		if(!$num){
 			return false;
@@ -35,7 +35,7 @@ class Pano2CubesCommand extends CConsoleCommand {
 			$pano_queue = new PanoQueue();
 			$pano_queue->update_lock($v, 1);
 		}
-		
+
 		foreach($pano_pics as $k=>$v){
 			$this->turn_to_cube($v);
 			//$this->update_item_state_lock($k);
@@ -55,21 +55,21 @@ class Pano2CubesCommand extends CConsoleCommand {
 		$path_1 = $prefix . '/' . 'script-1.txt';
 		$path_2 = $prefix . '/' . 'script-2.txt';
 
-			if(!file_exists($path_1)){
-				$num = 1;
-				file_put_contents($path_1, 1);
-				$this->script_path = $path_1;
-			}
-			elseif(!file_exists($path_2)){
-				$num = 2;
-				file_put_contents($path_2, 1);
-				$this->script_path = $path_2;
-			}
-			/* elseif(!file_exists($path_3)){
-				$num = 3;
-				file_put_contents($path_3, 1);
-				$this->script_path = $path_3;
-			} */
+		if(!file_exists($path_1)){
+			$num = 1;
+			file_put_contents($path_1, 1);
+			$this->script_path = $path_1;
+		}
+		elseif(!file_exists($path_2)){
+			$num = 2;
+			file_put_contents($path_2, 1);
+			$this->script_path = $path_2;
+		}
+		/* elseif(!file_exists($path_3)){
+		 $num = 3;
+		file_put_contents($path_3, 1);
+		$this->script_path = $path_3;
+		} */
 		return $num;
 	}
 	/**
@@ -79,81 +79,74 @@ class Pano2CubesCommand extends CConsoleCommand {
 		$this->cube($path);
 		$this->exec_libpano($path);
 	}
-	
+
 	public function cube($path){
 		$script = "p w{$this->width} h{$this->width} f0 v90 u20 n\"TIFF_m\"\n
-i n\"{$path}\"\n
-o f4 y0 r0 p0 v360\n
-i n\"{$path}\"\n
-o f4 y-90 r0 p0 v360\n
-i n\"{$path}\"\n
-o f4 y-180 r0 p0 v360\n
-i n\"{$path}\"\n
-o f4 y90 r0 p0 v360\n
-i n\"{$path}\"\n
-o f4 y0 r0 p-90 v360\n
-i n\"{$path}\"\n
-o f4 y0 r0 p90 v360";
+		i n\"{$path}\"\n
+		o f4 y0 r0 p0 v360\n
+		i n\"{$path}\"\n
+		o f4 y-90 r0 p0 v360\n
+		i n\"{$path}\"\n
+		o f4 y-180 r0 p0 v360\n
+		i n\"{$path}\"\n
+		o f4 y90 r0 p0 v360\n
+		i n\"{$path}\"\n
+		o f4 y0 r0 p-90 v360\n
+		i n\"{$path}\"\n
+		o f4 y0 r0 p90 v360";
 		echo $this->script_path;
-	        return file_put_contents($this->script_path, $script);
-	    }
-	    public function exec_libpano($path){
-	    	$str = "/usr/local/libpano13/bin/PTmender {$this->script_path}";
-	    	echo "----cube pano {$path}----\r\n";
-	    	echo $str;
-	    	//system($str);
-	    	echo "----cube pano down {$path}----\r\n";
-	    	//$this->covert();
-	    }
-	    public function covert(){
-	    $panos = array( 'pano0005'=>'bottom',
-	    	'pano0000'=>'front',
-	    			'pano0001'=>'right',
-	    			'pano0002'=>'back',
-	    					'pano0003'=>'left',
-	    					'pano0004'=>'top', );
-	    					foreach($panos as $k=>$v){
-	    					$old = $k.'.tif';
-	    					$new = $v.'.jpg';
-	    							echo "----covering tifToJpg {$old}----\n";
-	    									$this->tifToJpg($old, $new);
-	    									echo "----covering tifToJpg success {$old}----\n";
-	    									$this->move_cube_file($new);
-	    									}
-	    									}
-	    									public function move_cube_file($file){
-	    	$explode = explode('/', $this->split_file);
-	    	$num = count($explode)-2;
-	    	$new_folder = $this->cube_path.'/'.$explode[$num].'/cube';
-	    	$new_file = $new_folder.'/'.$file;
-	    		if(!file_exists($new_folder)){
-	    		mkdir($new_folder);
-	    	}
-	    
-	    	copy($file, $new_file);
-	    	unlink($file);
-	    	echo "....moving to {$new_file}\n";
-	    	}
-	    	public function tifToJpg($old, $new){
-	    	if(!file_exists($old)){
-	    	$this->error[] = $old;
-	    	return false;
-	    	}
-	    	$myimage = new Imagick($old);
-	    	$myimage->setImageFormat("jpeg");
-	    	$myimage->setCompressionQuality( 100 );
-	    	$myimage->writeImage($new);
-	    	$myimage->clear();
-	    	$myimage->destroy();
-	    		if(!file_exists($new)){
-	    		$this->error[] = $old;
-	    		return false;
-	    	}
-	    	}
-	    	
-	    	
-	    	
-	
+		return file_put_contents($this->script_path, $script);
+	}
+	public function exec_libpano($path){
+		$str = "/usr/local/libpano13/bin/PTmender {$this->script_path}";
+		echo "----cube pano {$path}----\r\n";
+		echo $str;
+		//system($str);
+		echo "----cube pano down {$path}----\r\n";
+		$this->covert($path);
+	}
+	public function covert($path){
+		$panos = array( 'pano0005'=>'bottom',
+				'pano0000'=>'front',
+				'pano0001'=>'right',
+				'pano0002'=>'back',
+				'pano0003'=>'left',
+				'pano0004'=>'top', );
+		foreach($panos as $k=>$v){
+			$old = $k.'.tif';
+			$new = $v.'.jpg';
+			$path_explode = explode('/', $path);
+			$prefix = '/';
+			for($i = 0; $i<count($path_explode)-2; $i++){
+				$prefix .= $path_explode[$i] . '/';
+			}
+			echo $prefix;
+			echo "----covering tifToJpg {$old}----\n";
+			//$this->tifToJpg($old, $new);
+			echo "----covering tifToJpg success {$old}----\n";
+			//$this->move_cube_file($new);
+		}
+	}
+	public function tifToJpg($old, $new){
+		if(!file_exists($old)){
+			$this->error[] = $old;
+			return false;
+		}
+		$myimage = new Imagick($old);
+		$myimage->setImageFormat("jpeg");
+		$myimage->setCompressionQuality( 100 );
+		$myimage->writeImage($new);
+		$myimage->clear();
+		$myimage->destroy();
+		if(!file_exists($new)){
+			$this->error[] = $old;
+			return false;
+		}
+	}
+
+
+
+
 	/**
 	 * 获取需处理的队列
 	 */
@@ -183,7 +176,7 @@ o f4 y0 r0 p90 v360";
 		$pano_queue = new PanoQueue();
 		return $pano_queue->update_lock($scene_id, 0);
 	}
-	
+
 	/**
 	 * 获取需处理图片
 	 */
