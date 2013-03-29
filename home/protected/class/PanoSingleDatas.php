31d03dd91a51f67303fbf816ac0796324351bbb8
wenbojx <wenbojx@sina.com>
2013-03-29 16:11:49 +0800

    a

--- home/protected/class/PanoSingleDatas.php
+++ home/protected/class/PanoSingleDatas.php
@@ -94,9 +94,9 @@ class PanoSingleDatas{
         }
         //$this->global_datas['s_attribute']['debug'] = 'true';
         // $this->global_datas['control']['s_attribute']['autorotation'] = 'enabled:false';
-        $this->global_datas['control']['s_attribute']['autorotation'] = 'enabled:true,delay:3';
+        $this->global_datas['control']['s_attribute']['autorotation'] = 'enabled:true,delay:5';
         if($this->display_config['auto']){
-        	$this->global_datas['control']['s_attribute']['autorotation'] = 'enabled:true,delay:3';
+        	$this->global_datas['control']['s_attribute']['autorotation'] = 'enabled:true,delay:5';
         }
         $this->global_datas['branding']['s_attribute']['visible'] = 'false';
         $this->global_datas['panoramas']['s_attribute']['firstPanorama'] = $this->panoram_pre.$this->scene_id;
