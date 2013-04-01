7acca1068c79068f0699c4fae9ee4aa9611bf04c
wenbojx <wenbojx@sina.com>
2013-04-01 14:39:33 +0800

    a

--- hunsha/protected/views/home/mobile.php
+++ hunsha/protected/views/home/mobile.php
@@ -7,30 +7,42 @@
 		<div class="clear"></div>
 		<div class="main_content">
 			<div class="item">
+				<a href="/home/huanjin">
 				<div class="radius item1 fleft">
 					<span>公司环境</span>
 				</div>
+				</a>
+				<a href="/home/about">
 				<div class="radius item2 fright">
 					<span>关于我们</span>
 				</div>
+				</a>
 			</div>
 			<div class="clear"></div>
 			<div class="item">
+				<a href="/home/case">
 				<div class="radius item3 fleft">
 					<span>案例展示</span>
 				</div>
+				</a>
+				<a href="/home/fanwei">
 				<div class="radius item4 fright">
 					<span>服<br>务<br>范<br>围</span>
 				</div>
+				</a>
 			</div>
 			<div class="clear"></div>
 			<div class="item">
+				<a href="/home/liuchen">
 				<div class="radius item5 fleft">
 					<span>服<br>务<br>流<br>程</span>
 				</div>
+				</a>
+				<a href="/home/lianxi">
 				<div class="radius item6 fright">
 					<span>联系我们</span>
 				</div>
+				</a>
 			</div>
 			<div class="clear"></div>
 		</div>
